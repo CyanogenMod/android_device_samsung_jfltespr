52234d0946829545ac8e4f5b042c72d409901afc
Paul Keith <javelinanddart@gmail.com>
2016-07-29 11:47:23 -0700

    jfltespr: Update Fingerprint
    * KK is ancient
    
    Change-Id: Iac34af5384a5b1f3ad526854203d935439dfa106
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- cm.mk
+++ cm.mk
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jfltespr \
     TARGET_DEVICE=jfltespr \
-    BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.4.2/KOT49H/L720VPUFNG2:user/release-keys" \
-    PRIVATE_BUILD_DESC="jfltespr-user 4.4.2 KOT49H L720VPUFNG2 release-keys"
+    BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:5.0.1/LRX22C/L720VPUGOK3:user/release-keys" \
+    PRIVATE_BUILD_DESC="jfltespr-user 5.0.1 LRX22C L720VPUGOK3 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
